c3962c6d2ac865d218a1b907515f06a1989709f2
Ayoub Mouloua <76871172+ayoub0x1@users.noreply.github.com>
2021-07-30 17:32:40 +0100

    Update ft_putnbr.c

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<unistd.h>
+#include <unistd.h>
 
 void	ft_putchar(char c)
 {
@@ -19,16 +19,21 @@ void	ft_putchar(char c)
 
 void	ft_putnbr(int nb)
 {
-	if (nb < 0)
+	long int	n;
+
+	n = nb;
+	if (n < 0)
 	{
 		ft_putchar('-');
-		nb = -nb;
+		n = -n;
 	}
-	if (nb >= 10)
+	if (n > 9)
 	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
 	}
 	else
-		ft_putchar(nb + '0');
+	{
+		ft_putchar(n + '0');
+	}
 }
